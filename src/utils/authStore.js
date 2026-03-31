5f7e2924fa347975cae2964e785fd783a2783247
shubham thakur <shubhamthakur60278@gmail.com>
2026-03-31 22:21:38 +0530

    feat: add auth store

---
+++ src/utils/authStore.js
@@ -0,0 +1,39 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      isAuthenticated: false,
+
+      setUser: (user) => set({ user }),
+      setToken: (token) => set({ token }),
+      setAuthenticated: (isAuthenticated) => set({ isAuthenticated }),
+
+      login: (user, token) => {
+        set({
+          user,
+          token,
+          isAuthenticated: true,
+        })
+        localStorage.setItem('token', token)
+      },
+
+      logout: () => {
+        set({
+          user: null,
+          token: null,
+          isAuthenticated: false,
+        })
+        localStorage.removeItem('token')
+      },
+    }),
+    {
+      name: 'auth-store',
+    }
+  )
+)
+
+export default useAuthStore
